7513c664650a96b8a43a28aa82c6f6b2b126c609
Joe <919946@swansea.ac.uk>
2020-11-21 15:56:22 +0000

    Added toggle to radio On/Off

---
+++ Assets/toggleRadio.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toggleRadio : MonoBehaviour
+{
+    public GameObject radioOn, radioOff;
+    public static bool radioIsOn = true;
+    public static bool radioIsOff = false;
+
+    public void turnOff()
+    {
+        if(radioIsOn)
+        {
+            radioOn.SetActive(false);
+            radioOff.SetActive(true);
+            radioIsOn = false;
+            radioIsOff = true;
+        }
+
+    }
+
+    public void turnOn()
+    {
+        if (radioIsOff)
+        {
+            radioOn.SetActive(true);
+            radioOff.SetActive(false);
+            radioIsOn = true;
+            radioIsOff = false;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
